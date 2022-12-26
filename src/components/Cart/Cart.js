5bc29ccc5206543fec47f0c9ad371f75876267f2
Blank67 <hmnshshrm67@gmail.com>
2022-12-26 16:21:22 +0530

    cart buttons logic added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,7 +10,7 @@ const Cart = (props) => {
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        {cartItems.map((itm) => (<CartItem key={itm.id} item={{ title: itm.title, quantity: itm.quantity, total: itm.totalPrice, price: itm.price }} />))}
+        {cartItems.map((itm) => (<CartItem key={itm.id} item={{ id: itm.id, title: itm.title, quantity: itm.quantity, total: itm.totalPrice, price: itm.price }} />))}
       </ul>
     </Card>
   );
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,15 +1,25 @@
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
 import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price, id } = props.item;
+  const dispatch = useDispatch();
+
+  const onAdd = () => {
+  dispatch(cartActions.addItem({id: id, price: price, title: title}));    
+  }
+  const onRemove = () => {
+    dispatch(cartActions.removeItem(id));
+  }
 
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
-          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+          Rs. {total.toFixed(2)}{' '}
+          <span className={classes.itemprice}>(Rs. {price.toFixed(2)}/item)</span>
         </div>
       </header>
       <div className={classes.details}>
@@ -17,8 +27,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={onRemove}>-</button>
+          <button onClick={onAdd}>+</button>
         </div>
       </div>
     </li>
