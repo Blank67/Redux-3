7a2c456699735494383f8c11b2948eeb88a8f661
Blank67 <hmnshshrm67@gmail.com>
2022-12-26 16:07:19 +0530

    cart count logic

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,41 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_DATA = [
+  {
+    id: 1,
+    price: 10,
+    title: "Book 1",
+    description: "This is a first product - amazing!"
+  },
+  {
+    id: 2,
+    price: 20,
+    title: "Book 2",
+    description: "This is a second product - amazing!"
+  },
+  {
+    id: 3,
+    price: 15,
+    title: "Book 3",
+    description: "This is a third product - amazing!"
+  },
+  {
+    id: 4,
+    price: 60,
+    title: "Book 4",
+    description: "This is a fourth product - amazing!"
+  }
+];
+
 const Products = (props) => {
+  const items = DUMMY_DATA.map((itm) => (<ProductItem key={itm.id} id={itm.id} title={itm.title} price={itm.price} description={itm.description} />))
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {items}
       </ul>
     </section>
   );
---
+++ src/store/cart-slice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    items: [],
+    totalQuantity: 0
+}
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: initialState,
+    reducers: {
+        addItem (state, action) {
+            const newItem = action.payload;
+            const existingItem = state.items.find((itm) => (itm.id === newItem.id));
+            state.totalQuantity++;
+            if(!existingItem){
+                state.items.push({itemId: newItem.id, price: newItem.price, quantity: 1, totalPrice: newItem.price, name: newItem.title });
+            }else{
+                existingItem.quantity++;
+                existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+            }
+        },
+        removeItem (state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find((itm) => (itm.id === id));
+            state.totalQuantity--;
+            if(existingItem.quantity === 1){
+                state.items = state.items.filter((itm) => (itm.id !== id));
+            }else{
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
+        }
+    }
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
